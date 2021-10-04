3b0835a677d81450f3705c119b74beed79888bb0
xuwu <zx597813039@gmail.com>
2021-10-04 20:05:45 +0800

    feat: init cli

---
+++ packages/cli/src/cli.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import chalk from 'chalk'
+import minimist from 'minimist'
+
+const argv: any = minimist(process.argv.slice(2))
+
+console.log(chalk.cyan(`ydoc v${require('../package.json').version}`))
+
+const command = argv._[0]
+
+if (!command || command === 'dev') {
+  // 启动个 server
+}
