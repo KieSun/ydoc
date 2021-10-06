f1727829f796e738c4ac4de1f9739079a012b888
xuwu <zx597813039@gmail.com>
2021-10-06 19:59:38 +0800

    feat: getCustomConfig

--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -1,3 +1,13 @@
+import path from 'path'
+import * as fs from 'fs'
+
 export async function resolveConfig(root: string) {
-  console.log(root)
+  const customConfig = await getCustomConfig(root)
+  console.log(customConfig)
+}
+
+export async function getCustomConfig(root: string) {
+  const configPath = path.resolve(root, 'ydoc.config.js')
+  const hasCustomConfig = fs.existsSync(configPath)
+  return hasCustomConfig ? require(configPath) : {}
 }
